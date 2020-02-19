62b52092f61285553901aee33a051ebc467011bc
violettabertone <violettabertone2@libero.it>
2020-02-19 17:31:05 +0100

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroescomponentshtmlComponent } from './heroes/heroescomponentshtml/heroescomponentshtml.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent],
+  declarations: [ AppComponent, HeroesComponent, HeroescomponentshtmlComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/heroes/heroescomponentshtml/heroescomponentshtml.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroescomponentshtml',
+  templateUrl: './heroescomponentshtml.component.html',
+  styleUrls: ['./heroescomponentshtml.component.css']
+})
+export class HeroescomponentshtmlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
